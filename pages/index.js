13f471ef7b72d1552fa83667e0d058a088c46bd5
Todor Cholakov <t.s.cholakov@gmail.com>
2023-08-07 16:24:00 +0300

    next installed

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import { createClient } from 'contentful'
+import Image from 'next/image'
+
+export async function getStaticProps(){
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  })
+
+
+    const res = await client.getEntries({content_type: 'images'})
+  return {
+    props: {images: res.items
+    }
+  }
+}
+export default function Home({images}) {
+console.log (images)
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+    </main>
+  )
+}
