3d041d84bf2cddddbcbd59e6cd16a26a6f06d59a
Todor Cholakov <t.s.cholakov@gmail.com>
2023-08-17 09:14:30 +0300

    ok

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-import { db } from "./components/utils/firebase";
+import { db } from "../utils/firebase"
 import { collection, addDoc } from "firebase/firestore"; 
 export default function Contact() {
 
