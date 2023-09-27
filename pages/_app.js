abc5ca0bb920721848f71e5bbfe5e448bc8fdc63
Todor Cholakov <t.s.cholakov@gmail.com>
2023-09-27 16:14:12 +0300

    analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import {NextUIProvider} from "@nextui-org/react";
-
+import { Analytics } from '@vercel/analytics/react';
 import NavbarMenuA from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
     <NavbarMenuA />
   
    <Component {...pageProps} />
-
+    <Analytics />
    <Footer />
 </NextUIProvider>
  )
