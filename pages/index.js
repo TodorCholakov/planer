524803c64ea825961a3d7ac21a194a593386dbb4
Todor Cholakov <t.s.cholakov@gmail.com>
2023-09-28 19:09:00 +0300

    new

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
-import { createClient } from "contentful";
 import React from "react";
 import { motion } from "framer-motion";
 import { titleAnimation, containerIndex } from "../animations";
-
-export async function getStaticProps() {
-  const client = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-  });
-
-  const res = await client.getEntries({ content_type: "images" });
-  return {
-    props: { images: res.items },
-  };
-}
 
 export default function Home({ images }) {
   return (
